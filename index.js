24d72123ad84519d6779338e77e247c2510eb876
ggoodman <ggoodman@gmail.com>
2014-06-02 16:55:44 -0400

    Export as an Angular.js module instead of object

--- index.js
+++ index.js
@@ -11,17 +11,13 @@ module.exports = function(content, sourceMap) {
 	var query = loaderUtils.parseQuery(this.query);
 	var exports = [];
 	var keys = Object.keys(query);
-	if(keys.length == 1 && typeof query[keys[0]] == "boolean") {
-		exports.push("module.exports = " + keys[0]);
-	} else {
-		keys.forEach(function(name) {
-			var mod = name;
-			if(typeof query[name] == "string") {
-				mod = query[name];
-			}
-			exports.push("exports[" + JSON.stringify(name) + "] = (" + mod + ");");
-		});
-	}
+	keys.forEach(function(name) {
+		var mod = name;
+		if(typeof query[name] == "string") {
+			mod = query[name];
+		}
+		exports.push("exports[" + JSON.stringify(name) + "] = angular.module(" + mod + ");");
+	});
 	if(sourceMap) {
 		var currentRequest = loaderUtils.getCurrentRequest(this);
 		var node = SourceNode.fromStringWithSourceMap(content, new SourceMapConsumer(sourceMap));
