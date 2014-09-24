816a0e9fff065c026d1c26e053f3c92ebe37bfb2
Geoff Goodman <ggoodman@gmail.com>
2014-09-23 21:46:44 -0400

    Add license, tweak naming

--- index.js
+++ index.js
@@ -2,21 +2,21 @@
 	MIT License http://www.opensource.org/licenses/mit-license.php
 	Author Tobias Koppers @sokra
 */
-var loaderUtils = require("loader-utils");
-var path = require("path");
 var SourceNode = require("source-map").SourceNode;
 var SourceMapConsumer = require("source-map").SourceMapConsumer;
-var FOOTER = "/*** EXPORTS FROM exports-loader ***/\n";
+var Utils = require("loader-utils");
+var FOOTER = "/*** EXPORTS FROM ng-loader ***/\n";
+
 module.exports = function(content, sourceMap) {
 	if(this.cacheable) this.cacheable();
-	var query = loaderUtils.parseQuery(this.query);
+	var query = Utils.parseQuery(this.query);
 	var exports = [];
 	var keys = Object.keys(query);
 	if (keys.length == 1) {
 			exports.push("module.exports = window.angular.module('" + keys[0] + "');");
 	}
 	if(sourceMap) {
-		var currentRequest = loaderUtils.getCurrentRequest(this);
+		var currentRequest = Utils.getCurrentRequest(this);
 		var node = SourceNode.fromStringWithSourceMap(content, new SourceMapConsumer(sourceMap));
 		node.add("\n\n" + FOOTER + exports.join("\n"));
 		var result = node.toStringWithSourceMap({
@@ -26,4 +26,4 @@ module.exports = function(content, sourceMap) {
 		return;
 	}
 	return content + "\n\n" + FOOTER + exports.join("\n");
-}
+};
